e80f91e95d5abc6d38af9180dbb1124ddcd5dec8
Serg Krivoblotsky <krivoblotsky@macpaw.com>
2018-09-12 15:05:13 +0300

    Initial commit

---
+++ PermissionsKit/PermissionsKit.h
@@ -0,0 +1,19 @@
+//
+//  PermissionsKit.h
+//  PermissionsKit
+//
+//  Created by Sergii Kryvoblotskyi on 9/12/18.
+//  Copyright © 2018 MacPaw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PermissionsKit.
+FOUNDATION_EXPORT double PermissionsKitVersionNumber;
+
+//! Project version string for PermissionsKit.
+FOUNDATION_EXPORT const unsigned char PermissionsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PermissionsKit/PublicHeader.h>
+
+
