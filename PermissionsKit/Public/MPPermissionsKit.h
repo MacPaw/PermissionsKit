ff266a2892477219be483efbf3e29c2bc0b53a15
Serg Krivoblotsky <krivoblotsky@macpaw.com>
2018-09-17 11:07:50 +0300

    Comments added

--- PermissionsKit/Public/MPPermissionsKit.h
+++ PermissionsKit/Public/MPPermissionsKit.h
@@ -17,19 +17,21 @@ NS_SWIFT_NAME(PermissionsKit)
 @interface MPPermissionsKit : NSObject
 
 /**
- Requests current authorization status for a given type.
-
- @param permissionType MPPermissionType
- @return MPAuthorizationStatus
+ * Requests current authorization status for a given type.
+ *
+ * @discussion It is safe to call this method on system where permission type is not supported. MPAuthorizationStatusAuthorized will be returned.
+ *
+ * @param permissionType MPPermissionType
+ * @return MPAuthorizationStatus
  */
 + (MPAuthorizationStatus)authorizationStatusForPermissionType:(MPPermissionType)permissionType;
 
 /**
- Requests user authorization for a given permission. Completion will be invoked with a user decision. Completion queue is undefined.
- Important Note: The will be no completion when requesting MPPermissionTypeFullDiskAccess, because its status is unknown. You should implement your own callback mechanism, for example - polling authorization.
-
- @param permissionType MPPermissionType
- @param completionHandler void (^)(MPAuthorizationStatus status)
+ * Requests user authorization for a given permission. Completion will be invoked with a user decision. Completion queue is undefined.
+ * @discussion The will be no completion when requesting MPPermissionTypeFullDiskAccess, because its status is unknown. You should implement your own callback mechanism, for example - polling authorization. It is safe to call this method on system where permission type is not supported. MPAuthorizationStatusAuthorized will be returned.
+ *
+ * @param permissionType MPPermissionType
+ * @param completionHandler void (^)(MPAuthorizationStatus status)
  */
 + (void)requestAuthorizationForPermissionType:(MPPermissionType)permissionType withCompletion:(void (^)(MPAuthorizationStatus status))completionHandler;
 
